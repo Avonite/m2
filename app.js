3f88a797dc2be5ae1ffdb749cc2732ecf2a90058
Marius Birkhoff <m.birkhoff@cleverstrategy.nl>
2018-12-03 14:49:49 +0100

    Restructuring of files and installation of EJS

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 var express = require("express");
 var http = require("http");
+var path = require("path");
 
 var port = process.argv[2];
 var app = express();
 
-var indexRouter = require('./routes/index');
 
-app.use('/', indexRouter);
+app.set('views', path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
 
+// implement routes
+var router = require('./routes/game');
+app.use('/', router);
+
+// serve static files
 app.use(express.static(__dirname + "/public"));
+
+// Listen for incoming requests
 http.createServer(app).listen(port);
