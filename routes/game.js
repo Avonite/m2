0c172ea84fb9a30619f0146a11c4f5c54c12faf1
Marius Birkhoff <m.birkhoff@cleverstrategy.nl>
2018-12-05 16:05:38 +0100

    Moved Controllers

--- routes/game.js
+++ routes/game.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
-var game_controller     = require("../controllers/gameController");
-var splash_controller   = require("../controllers/splashController");
+var game_controller     = require("../app/controllers/gameController");
+var splash_controller   = require("../app/controllers/splashController");
 
 /* GET Splash Screen */
 router.get("/splash", splash_controller.show);
