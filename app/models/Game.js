c3b4b8d53f64a675bda43756beeac6185ef74e30
Marius Birkhoff <m.birkhoff@cleverstrategy.nl>
2018-12-14 12:46:42 +0100

    Fixed the problemo

--- app/models/Game.js
+++ app/models/Game.js
@@ -48,6 +48,27 @@ function Game(maker){
     return this.active;
   }
 
+  this.reverse = function(){
+    this.maker.getWebSocket()._events = {};
+    this.braker.getWebSocket()._events = {};
+
+    var maker = this.maker;
+    this.maker = this.braker;
+    this.braker = maker;
+
+    this.maker.getWebSocket().on("message", receiveFromMaker.bind(this));
+    this.maker.getWebSocket().on("close", makerDisconnected.bind(this));
+
+    this.braker.getWebSocket().on("message", receiveFromBraker.bind(this));
+    this.braker.getWebSocket().on("close", brakerDisconnected.bind(this));
+
+    this.maker.getWebSocket().send(JSON.stringify({action: "resetGame", props: {}}));
+    this.braker.getWebSocket().send(JSON.stringify({action: "resetGame", props: {}}));
+
+    this.maker.getWebSocket().send(JSON.stringify({action: "yourRole", props: {role: "maker"}}));
+    this.braker.getWebSocket().send(JSON.stringify({action: "yourRole", props: {role: "braker"}}));
+  }
+
 }
 
 function makerDisconnected(){
@@ -93,6 +114,37 @@ function receiveFromMaker(data){
         action: 'codeReady',
         props: {}
       }));
+    break;
+
+    case "verifiedPinline":
+      this.braker.getWebSocket().send(JSON.stringify({
+        action: 'verifiedPinline',
+        props: data.props
+      }));
+    break;
+
+    case "brakerWins":
+      this.braker.getWebSocket().send(JSON.stringify({
+        action: 'brakerWins',
+        props: data.props
+      }));
+
+      var me = this;
+      setTimeout(function(){
+        me.reverse();
+      }, 3000);
+    break;
+
+    case "brakerLoses":
+      this.braker.getWebSocket().send(JSON.stringify({
+        action: 'brakerLoses',
+        props: data.props
+      }));
+
+      var me = this;
+      setTimeout(function(){
+        me.reverse();
+      }, 3000);
     break;
 
     default:
